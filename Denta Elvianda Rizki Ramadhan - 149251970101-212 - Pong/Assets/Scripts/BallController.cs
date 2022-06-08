f3b401b1418c064f4dc43736a223900e2aac131a
dentaelvianda <dentaelvianda@gmail.com>
2022-06-09 04:11:01 +0800

    Base Gameplay
    add Base gameplay and Code

---
+++ Denta Elvianda Rizki Ramadhan - 149251970101-212 - Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+}
